50886d57d202c5dc016d7fc70d0b07d8e6b9cf74
Christian Prather <clemsbach@gmail.com>
2023-12-05 23:14:14 -0700

    Added noisy quantum virtual machine (weber) architecture

---
+++ qvm/weber.py
@@ -0,0 +1,36 @@
+# Google Quantum computer library
+import cirq
+import cirq_google
+import qsimcirq
+# Argument parsing
+import argparse
+# Plotting results
+import matplotlib.pyplot as plt
+
+PROCESSOR_ID = "weber"
+NUM_SIMULATIONS = 3000
+
+
+def weber():
+    calibration_noise = cirq_google.engine.load_median_device_calibration(
+        PROCESSOR_ID)
+
+    noise_properties = cirq_google.noise_properties_from_calibration(
+        calibration_noise)
+
+    noise_model = cirq_google.NoiseModelFromGoogleNoiseProperties(
+        noise_properties)
+
+    simulator = qsimcirq.QSimSimulator(noise=noise_model)
+
+    device = cirq_google.engine.create_device_from_processor_id(PROCESSOR_ID)
+
+    virtual_processor = cirq_google.engine.SimulatedLocalProcessor(
+        processor_id=PROCESSOR_ID, sampler=simulator, device=device, calibrations={calibration_noise.timestamp // 1000: calibration_noise})
+
+    simulation_engine = cirq_google.engine.SimulatedLocalEngine([
+        virtual_processor])
+
+    print("Quantum Virtual Machine {} is ready! QBit grid: {}".format(
+        PROCESSOR_ID, simulation_engine.get_processor(PROCESSOR_ID).get_device()))
+    return simulation_engine
